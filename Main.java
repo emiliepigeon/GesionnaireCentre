53a5525674f177acaa5b87323e8abee8a6a750fa
emilie_pigeon <emiliepigeon@free.fr>
2025-02-12 15:59:05 +0100

    Initiation du projet

---
+++ Main.java
@@ -0,0 +1,9 @@
+// Je suis le fichier principal qui lance mon programme
+// Je n'ai pas besoin de package car je suis à la racine du projet
+
+public class Main {
+    // La méthode main est le point de départ de mon programme
+    public static void main(String[] args) {
+        // J'appelle
+    }
+}
---
+++ gestionnaire/Personne.java
@@ -0,0 +1,7 @@
+// Je déclare que cette classe fait partie du package gestionnaire
+package gestionnaire;
+
+public class Personne {
+    // Je déclare les attributs de la classe
+    // Je crée les méthodes
+}
