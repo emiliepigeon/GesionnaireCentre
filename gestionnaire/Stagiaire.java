96361d4d12a4d94ea6bce32cf445bc3bf9261cd2
emilie_pigeon <emiliepigeon@free.fr>
2025-02-13 12:18:44 +0100

    classe fille stagiaire v1

--- gestionnaire/Stagiaire.java
+++ gestionnaire/Stagiaire.java
@@ -1,7 +1,15 @@
 // Je déclare que cette classe fait partie du package gestionnaire
 package gestionnaire;
 
-public class Stagiaire extends Personne {
-    // Je déclare les attributs de la classe
-    // Je crée les méthodes
+public class Personne {
+    // Je déclare les attributs de la classe:
+	    // nom
+	    // prenom
+	    // mail
+	    // centre
+	    // formations
+    // Je crée les méthodes:
+	    // toString()
+			// Getters:
+			// Setters uniquement si néceassaires: 
 }
