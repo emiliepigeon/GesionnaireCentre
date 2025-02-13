2187d5f73b11e7509fb8c990f7ba87b0f7734c02
emilie_pigeon <emiliepigeon@free.fr>
2025-02-13 12:14:44 +0100

    class Centre v1

---
+++ gestionnaire/Centre.java
@@ -0,0 +1,24 @@
+// Je déclare que cette classe fait partie du package gestionnaire
+package gestionnaire;
+
+public class Centre {
+    // Je déclare les attributs de la classe:
+	    // numCentre
+	    // libelle
+	    // responsable
+	    // adresse
+	    // codePostal
+	    // ville
+	    // formations
+	    // formateurs
+	    // stagiaires
+    // Je crée les méthodes:
+	    // ListeFormations()
+		  // ListeFormateurs()
+		  // ListeStagiaires()
+		  // AjouterFormation()
+		  // InscriptionFormateur()
+		  // InscriptionStagiaire()
+			// Getters:
+			// Setters uniquement si néceassaires:
+}
