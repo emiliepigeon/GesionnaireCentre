cf175ff1a36b30883a260d1fd442748d0be28e1f
emilie_pigeon <emiliepigeon@free.fr>
2025-02-13 12:16:08 +0100

    classe formation v1

--- gestionnaire/Formation.java
+++ gestionnaire/Formation.java
@@ -2,6 +2,20 @@
 package gestionnaire;
 
 public class Formation {
-    // Je déclare les attributs de la classe
-    // Je crée les méthodes
+    // Je déclare les attributs de la classe:
+	    // numOffre
+	    // libelle
+	    // dateDébut
+	    // dateFin
+	    // centre
+	    // formateurs
+	    // stagiaires
+    // Je crée les méthodes:
+	    // toString()
+		  // ListeFormateurs()
+		  // ListeStagiaires()
+		  // InscriptionFormateur()
+		  // InscriptionStagiaire()
+			// Getters:
+			// Setters uniquement si néceassaires:        
 }
